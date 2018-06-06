61ccb99780077d9e1bb84fb087dae7d7c2840164
Yogesh Walia <ywyogesh0@gmail.com>
2018-06-06 15:51:01 +0530

    First Commit

---
+++ src/main/java/com/ywyogesh0/kstreams/WordCountApp.java
@@ -0,0 +1,48 @@
+package com.ywyogesh0.kstreams;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.StreamsMetadata;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class WordCountApp {
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-1");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KTable<String, Long> kTable = builder.<String, String>stream("wc-input-1")
+                .mapValues(text -> text.toLowerCase())
+                .flatMapValues(lowerCaseText -> Arrays.asList(lowerCaseText.split(" ")))
+                .selectKey((ignoredKey, word) -> word)
+                .groupByKey()
+                .count(Materialized.as("Word_Count"));
+
+        kTable.toStream().to("wc-output-1");
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), properties);
+        streams.start();
+
+        for (StreamsMetadata metadata : streams.allMetadataForStore("Word_Count")) {
+            System.out.println(metadata.toString());
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
