e3b7fef23926b2620d81bc12950cc331733bb1b0
Yogesh Walia <yogeshwalia@Yogeshs-MacBook-Air.local>
2018-06-06 23:35:48 +0530

    Bug Fix 1.1

--- src/main/java/com/ywyogesh0/kstreams/WordCountApp.java
+++ src/main/java/com/ywyogesh0/kstreams/WordCountApp.java
@@ -2,11 +2,14 @@ package com.ywyogesh0.kstreams;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.state.StreamsMetadata;
 
 import java.util.Arrays;
@@ -27,14 +30,14 @@ public class WordCountApp {
 
         StreamsBuilder builder = new StreamsBuilder();
 
-        KTable<String, Long> kTable = builder.<String, String>stream("wc-input-1")
+        KTable<String, Long> kTable = builder.stream("wc-input-1", Consumed.with(Serdes.String(), Serdes.String()))
                 .mapValues(text -> text.toLowerCase())
                 .flatMapValues(lowerCaseText -> Arrays.asList(lowerCaseText.split(" ")))
                 .selectKey((ignoredKey, word) -> word)
                 .groupByKey()
                 .count(Materialized.as("Word_Count"));
 
-        kTable.toStream().to("wc-output-1");
+        kTable.toStream().to("wc-output-1", Produced.with(Serdes.String(), Serdes.Long()));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), properties);
         streams.start();
